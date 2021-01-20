cffa0815c75cd57e359ea09113632de89dd003b9
lemon.li <leli@suse.com>
2021-01-20 11:55:06 +0800

    Enlarge timeout of validate_script_output for curl_https

--- tests/console/curl_https.pm
+++ tests/console/curl_https.pm
@@ -28,7 +28,7 @@ sub run {
     my $max_retries = 7;
     for (1 .. $max_retries) {
         eval {
-            validate_script_output('curl -f -v https://httpbin.org/get 2>&1', sub { m,subjectAltName:[\w\s]+["]?httpbin.org["]? matched, });
+            validate_script_output('curl -f -v https://httpbin.org/get 2>&1', sub { m,subjectAltName:[\w\s]+["]?httpbin.org["]? matched, }, timeout => 180);
         };
         last unless ($@);
         diag "curl -f -v https://eu.httpbin.org/get failed: $@";
