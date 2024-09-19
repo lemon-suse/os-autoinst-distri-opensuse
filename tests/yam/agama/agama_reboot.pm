30e9fdffc6aac88aa4fb58bd5b4768c40f416458
lemon.li <leli@suse.com>
2024-09-19 02:22:38 -0400

    Save agama log before reboot after installation

--- tests/yam/agama/agama_reboot.pm
+++ tests/yam/agama/agama_reboot.pm
@@ -5,24 +5,16 @@
 # using a web automation tool to test directly from the Live ISO.
 # Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
 
-use base Yam::Agama::agama_base;
+use base "opensusebasetest";
 use strict;
 use warnings;
 use testapi;
 
-use testapi qw(
-  assert_script_run
-  get_required_var
-);
-
 sub run {
-    my $self = shift;
-    my $test = get_required_var('AGAMA_TEST');
     my $reboot_page = $testapi::distri->get_reboot_page();
 
-    script_run("dmesg --console-off");
-    assert_script_run("/usr/share/agama/system-tests/" . $test . ".cjs", timeout => 1200);
-    script_run("dmesg --console-on");
+    select_console('installation');
+    $reboot_page->reboot();
 }
 
 1;
