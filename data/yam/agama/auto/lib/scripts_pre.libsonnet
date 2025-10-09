b0b99cf29f0ef6d03e2f880ce7bd04aeaffc25da
lemon.li <leli@suse.com>
2025-10-09 05:30:42 -0400

    Debug bsc#1250662

--- data/yam/agama/auto/lib/scripts_pre.libsonnet
+++ data/yam/agama/auto/lib/scripts_pre.libsonnet
@@ -14,6 +14,12 @@
     name: 'wipefs',
     content: |||
       #!/usr/bin/env bash
+      lsdasd -a
+      vmcp query dasd
+      chzdev -e 150
+      sleep 10
+      lsdasd -a
+      vmcp query dasd
       for i in `lsblk -n -l -o NAME -d -e 7,11,254`
           do wipefs -af /dev/$i
           sleep 1
--- lib/Yam/Agama/agama_base.pm
+++ lib/Yam/Agama/agama_base.pm
@@ -27,6 +27,13 @@ sub upload_agama_logs {
     script_run("agama logs store -d /tmp/agama-logs", {timeout => 60});
     upload_logs("/tmp/agama-logs.tar.gz", failok => 1);
     save_and_upload_log('journalctl -b > /tmp/journal.log', "/tmp/journal.log", {timeout => 60});
+    
+    save_and_upload_log('lsdasd -a > /tmp/lsdasd.log', "/tmp/lsdasd.log", {timeout => 60});
+    save_and_upload_log('vmcp query dasd > /tmp/vmcp-dasd.log', "/tmp/vmcp-dasd.log", {timeout => 60});
+    save_and_upload_log('journal -u agama > /tmp/journal-agama.log', "/tmp/journal-agama.log", {timeout => 60});
+    save_and_upload_log('journal -u agama-web-server > /tmp/journal-agama-web-server.log', "/tmp/journal-agama-web-server.log", {timeout => 60});
+    script_run("chzdev -e 150", {timeout => 60});
+    save_and_upload_log('lsdasd -a > /tmp/after_lsdasd.log', "/tmp/after_lsdasd.log", {timeout => 60});
 
     # logs from the UI saved by default to this path
     upload_logs("/root/Downloads/agama-logs.tar.gz", log_name => 'agama-logs-from-ui.tar.gz', failok => 1);
