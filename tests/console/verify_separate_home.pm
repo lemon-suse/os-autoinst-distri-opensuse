84386d6093692e2fd55e0a48f53707e7899ab80a
lemon.li <leli@suse.com>
2026-09-02 22:34:56 -0400

    Check for separate home mount

--- tests/console/verify_separate_home.pm
+++ tests/console/verify_separate_home.pm
@@ -14,6 +14,7 @@ use testapi;
 
 sub run {
     select_console 'root-console';
+    assert_script_run("cat /etc/fstab");
 
     assert_script_run("lsblk -n | grep '/home'",
         fail_message => "Fail!\n
